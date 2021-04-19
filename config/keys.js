e1dd9eea20f0a1f168f894ce50babb657b5b912f
cmcmone <cmcmone@gmail.com>
2021-04-18 23:34:13 -0400

    Add db keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const user = 'koaapi';
+const password = 'EyKLgrgCzXd3ECSl';
+const mongodbURI = `mongodb+srv://${user}:${password}@koa-api.tumcx.mongodb.net/koa-api?retryWrites=true&w=majority`;
+
+module.exports = { mongodbURI };
